bfb82b29af6687c6ef2a6c9978d1eaee652977aa
salamalala <hello@ninaregli.ch>
2015-07-09 10:46:17 +0100

    fix svg and tabs

--- front-page.php
+++ front-page.php
@@ -34,7 +34,7 @@
 
            <img src=
             "<?php  
-            echo get_template_directory_uri(); ?>/assets/dist/images/logo.svg" 
+            echo get_template_directory_uri(); ?>/assets/dist/images/logo_def.svg" 
             alt="Logo Sans Papier Beratungsstelle"
             > 
 
