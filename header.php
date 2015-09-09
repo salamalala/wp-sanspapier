6472b701ee007661a47318461b9450a5cba56ffa
salamalala <hello@ninaregli.ch>
2015-09-09 07:56:45 +0100

    fix(header): add block to header large

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
 				</div>
 
 				<div class="block-nav menu-right ">
-           <a href="#" class="navigation-menu-button " id="js-mobile-menu">
+           <a href="#" class="menu-button " id="js-mobile-menu">
            <span class="menu-size">Menu</span>
 
            <div class="hamburger-icon">
