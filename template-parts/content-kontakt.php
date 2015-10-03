ce7850499d810a71aa55cbd337f88e0f119e74cc
salamalala <hello@ninaregli.ch>
2015-10-03 12:55:32 +0100

    feat(footer): finish footer incl social media logos

--- template-parts/content-kontakt.php
+++ template-parts/content-kontakt.php
@@ -7,7 +7,19 @@ get_header();
 
 <?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-<?php echo do_shortcode( '[contact-form-7 id="530" title="Kontaktformular"]' ); ?>
+<div class="fullwidth-box box pink-box contact">
+  <div class="contact-information">
+    <h1>Kontakt: </h1>
+    <p> Verein Berner Beratungsstelle für Sans-Papiers </p>
+    <p> Eigerplatz 5 </p>
+    <p> 3007 Bern </p>
+    <p> Tel: 031 385 18 27</p>
+    <p> Email: beratung@sans-papiers-contact.ch <p>
+  </div>
+  <div class="contact-form">
+    <?php echo do_shortcode( '[contact-form-7 id="530" title="Kontaktformular"]' ); ?>    
+  </div>
+</div>
 
 
 <?php   get_footer();   ?>
