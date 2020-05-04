6045af56c35425e724ed31913f02c21300f8c9c9
salamalala <hello@ninaregli.com>
2020-05-04 22:21:44 +0200

    feat(contact): only add spacing to contact email

--- template-parts/content-kontakt.php
+++ template-parts/content-kontakt.php
@@ -14,7 +14,7 @@ get_header();
     <p> Effingerstrasse 35 </p>
     <p> 3008 Bern </p>
     <p> Tel: 031 382 00 15</p>
-    <a href="mailto:beratung@sanspapiersbern.ch">Email: beratung@sanspapiersbern.ch </a>
+    <a href="mailto:beratung@sanspapiersbern.ch" class="contact-information__email">Email: beratung@sanspapiersbern.ch </a>
     <p> Fax: 031 382 00 18 <p>
   </div>
 
